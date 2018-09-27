ac298c421137f18c000a9b865ac65e4278aa0b1a
JeongTaek Han <smart8612@gmail.com>
2018-09-27 15:02:22 +0900

    Add Login && Sign up view

---
+++ moim/moim/InitialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.swift
+//  moim
+//
+//  Created by JeongTaek Han on 27/09/2018.
+//  Copyright © 2018 teammoim. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
