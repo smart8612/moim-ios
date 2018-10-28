2e20eb9f6d13d187ef382ef06605e68404a220eb
JeongTaek Han <smart8612@gmail.com>
2018-10-28 17:32:27 +0900

    Add Friend Table View Template

---
+++ moim/moim/ViewController/Friend/FriendTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  FriendTableViewCell.swift
+//  moim
+//
+//  Created by JeongTaek Han on 28/10/2018.
+//  Copyright © 2018 teammoim. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    
+    @IBOutlet var friendProfileImage: UIImageView!
+    
+    @IBOutlet var friendNameLabel: UILabel!
+    
+    @IBOutlet var friendStatusLabel: UILabel!
+    
+    @IBOutlet var friendAddButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
