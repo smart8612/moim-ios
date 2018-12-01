b2f72ced0976d9636773f66fa5e76c68aa1e0bc6
JeongTaek Han <smart8612@gmail.com>
2018-12-02 06:02:10 +0900

    Sync Event Data to AR View By Using ARkit + CoreLocation Library

--- moim/moim/ViewController/TabBarVC/AREventViewController.swift
+++ moim/moim/ViewController/TabBarVC/AREventViewController.swift
@@ -8,77 +8,132 @@
 
 import UIKit
 import ARCL
-import ARKit
-import SceneKit
 import CoreLocation
 import GLTFSceneKit
+import Firebase
 
-class AREventViewController: UIViewController {
+class AREventViewController: UIViewController, CLLocationManagerDelegate {
     
-//    var sceneLocationView = SceneLocationView()
-    @IBOutlet weak var AREventSceneView: ARSCNView!
+    var sceneLocationView = SceneLocationView()
+    var events = [Event]()
+    
+    var locationManager: CLLocationManager!
+    
+    var initialLocation: CLLocation!
+    var searchRadius: CLLocationDistance!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        // Set the view's delegate
-        AREventSceneView.delegate = self as? ARSCNViewDelegate
-        
-        // Show statistics such as fps and timing information
-        AREventSceneView.showsStatistics = true
-        
-        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        var scene: SCNScene
-        do {
-            let sceneSource = try GLTFSceneSource(named: "art.scnassets/ar-modeling/Marker.gltf")
-            scene = try sceneSource.scene()
-        } catch {
-            print("\(error.localizedDescription)")
-            return
-        }
-        
-        // Set the scene to the view
-        AREventSceneView.scene = scene
+        self.intializeARView()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        eventDataInitializer()
         
-        // Run the AREventViewSession
-        AREventSceneView.session.run(configuration)
-        
-//        sceneLocationView.run()
-//        view.addSubview(sceneLocationView)
-//
-//        let coordinate = CLLocationCoordinate2D(latitude: 37.5513, longitude: 126.9889)
-//        let location = CLLocation(coordinate: coordinate, altitude: 25)
-//        let image = UIImage(named: "like")!
-//        let annotationNode = LocationAnnotationNode(location: location, image: image)
-//
-//        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        sceneLocationView.run()
+        view.addSubview(sceneLocationView)
         
     }
     
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-//        sceneLocationView.frame = view.bounds
+        sceneLocationView.frame = view.bounds
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
-        AREventSceneView.session.pause()
-//        sceneLocationView.pause()
+        sceneLocationView.pause()
     }
+    
+    func addPinToARView() {
+        for event in self.events {
+            let coordinate = event.coordinate
+            guard let altitude = self.locationManager.location?.altitude else { return }
+            let location = CLLocation(coordinate: coordinate, altitude: altitude)
+            let image = UIImage(named: "pin")!
+            let annotationNode = LocationAnnotationNode(location: location, image: image)
+            
+            sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        
+        }
+    }
+    
+    private func eventDataInitializer() {
+        let eventRef = Database.database().reference().child("events")
+        
+        eventArrayReset()
+        
+        eventRef.observe(.value, with: { snapshot in
+            for child in snapshot.children.allObjects {
+                let event = child as! DataSnapshot
+                let eventDic = event.value as! Dictionary<String, Any>
+                
+                // Get Event Loaction
+                guard let latitude = eventDic["latitude"] else { return }
+                guard let longitude = eventDic["longitude"] else { return }
+                
+                let doubleLatitude = Double(latitude as! String)
+                let doubleLongitude = Double(longitude as! String)
+                
+                let coordinate = CLLocationCoordinate2D(latitude: doubleLatitude!, longitude: doubleLongitude!)
+                
+                // Get Event Data
+                guard let title = eventDic["title"] as? String else { return }
+                guard let text = eventDic["text"] as? String else { return }
+                guard let uid = eventDic["uid"] as? String else { return }
+                
+                // Generate New Event
+                let tmpEvent = Event(eventId: event.key, coordinate: coordinate, title: title, text: text, url: String(), uid: uid)
+                
+                self.events.append(tmpEvent)
+                
+            }
+            self.addPinToARView()
+            
+        })
+    }
+    
+    private func eventArrayReset() {
+        events.removeAll()
+    }
+    
+    private func intializeARView() {
+        setUpLocationManager()
+        setUpARView()
+        getCurrentLocation()
+        
+    }
+    
+    private func getCurrentLocation() {
+        if let coor = locationManager.location?.coordinate {
+            self.initialLocation = CLLocation(latitude: coor.latitude, longitude: coor.longitude)
+            self.searchRadius = 500
+            
+        }
+    }
+    
+    private func setUpLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    private func setUpARView() {
+        sceneLocationView.run()
+        sceneLocationView.showAxesNode = true
+        view.addSubview(sceneLocationView)
+    }
+
     
 }
 
